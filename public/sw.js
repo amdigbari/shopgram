523c61a4bb866e2b9486e0e824919714f31b917c
amdigbari <amirmahdidigbari@yahoo.com>
2020-04-28 19:09:18 +0430

    add simple usage for workbox-cacheable-response

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,18 @@
 import { registerRoute } from 'workbox-routing';
 import { precacheAndRoute } from 'workbox-precaching';
 import { NetworkFirst } from 'workbox-strategies';
+import { CacheableResponsePlugin } from 'workbox-cacheable-response';
 
-registerRoute(/^https?.*/, new NetworkFirst({ cacheName: 'offline-cache' }));
+registerRoute(
+    /^https?.*/,
+    new NetworkFirst({
+        cacheName: 'offline-cache',
+        plugins: [
+            new CacheableResponsePlugin({
+                statuses: [0, 200],
+            }),
+        ],
+    }),
+);
 
 precacheAndRoute(self.__WB_MANIFEST);
